614036e7c58abf4a59182ce83a9dfb7d3967377d
Shuhui Luo <shuhui@aperture.finance>
2023-03-03 12:34:12 -0700

    Add keeper register function

--- contracts/polygon/LimitOrderChainlink.sol
+++ contracts/polygon/LimitOrderChainlink.sol
@@ -2,15 +2,99 @@
 pragma solidity ^0.7.0;
 pragma abicoder v2;
 
-import "@chainlink/contracts/src/v0.7/interfaces/KeeperCompatibleInterface.sol";
+import {AutomationRegistryInterface, State, Config} from "./interfaces/AutomationRegistryInterface1_2.sol";
+import {LinkTokenInterface} from "@chainlink/contracts/src/v0.7/interfaces/LinkTokenInterface.sol";
+import {KeeperCompatibleInterface} from "@chainlink/contracts/src/v0.7/interfaces/KeeperCompatibleInterface.sol";
 
 import "./UniV3Automan.sol";
 
+interface KeeperRegistrarInterface {
+    function register(
+        string memory name,
+        bytes calldata encryptedEmail,
+        address upkeepContract,
+        uint32 gasLimit,
+        address adminAddress,
+        bytes calldata checkData,
+        uint96 amount,
+        uint8 source,
+        address sender
+    ) external;
+}
+
 contract LimitOrderChainlink is KeeperCompatibleInterface, UniV3Automan {
+    LinkTokenInterface public immutable i_link;
+    address public immutable registrar;
+    AutomationRegistryInterface public immutable i_registry;
+    bytes4 registerSig = KeeperRegistrarInterface.register.selector;
+
     constructor(
+        LinkTokenInterface _link,
+        address _registrar,
+        AutomationRegistryInterface _registry,
         INonfungiblePositionManager nonfungiblePositionManager,
         address V3_FACTORY
-    ) UniV3Automan(nonfungiblePositionManager, V3_FACTORY) {}
+    ) UniV3Automan(nonfungiblePositionManager, V3_FACTORY) {
+        i_link = _link;
+        registrar = _registrar;
+        i_registry = _registry;
+    }
+
+    /// @param name	Name of Upkeep
+    /// @param encryptedEmail Not in use in programmatic registration. Please specify with 0x
+    /// @param upkeepContract Address of Keepers-compatible contract that will be automated
+    /// @param gasLimit	The maximum amount of gas that will be used to execute your function on-chain
+    /// @param adminAddress	Address for Upkeep administrator. Upkeep administrator can fund contract.
+    /// @param checkData ABI-encoded fixed and specified at Upkeep registration and used in every checkUpkeep. Can be empty (0x)
+    /// @param amount The amount of LINK (in Wei) to fund your Upkeep. The minimum amount is 5 LINK. To fund 5 LINK please set this to 5000000000000000000
+    /// @param source Not in use in programmatic registration. Please specify with 0.
+    /// @return upkeepID
+    function registerAndPredictID(
+        string memory name,
+        bytes memory encryptedEmail,
+        address upkeepContract,
+        uint32 gasLimit,
+        address adminAddress,
+        bytes memory checkData,
+        uint96 amount,
+        uint8 source
+    ) internal returns (uint256 upkeepID) {
+        (State memory state, Config memory _c, address[] memory _k) = i_registry
+            .getState();
+        uint256 oldNonce = state.nonce;
+        bytes memory payload = abi.encode(
+            name,
+            encryptedEmail,
+            upkeepContract,
+            gasLimit,
+            adminAddress,
+            checkData,
+            amount,
+            source,
+            address(this)
+        );
+
+        i_link.transferAndCall(
+            registrar,
+            amount,
+            abi.encodeWithSelector(registerSig, payload)
+        );
+        (state, _c, _k) = i_registry.getState();
+        uint256 newNonce = state.nonce;
+        if (newNonce == oldNonce + 1) {
+            upkeepID = uint256(
+                keccak256(
+                    abi.encodePacked(
+                        blockhash(block.number - 1),
+                        address(i_registry),
+                        uint32(oldNonce)
+                    )
+                )
+            );
+        } else {
+            revert("auto-approve disabled");
+        }
+    }
 
     /**
      * @notice method that is simulated by the keepers to see if the limit order
