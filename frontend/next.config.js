0304e9745cdcd407268fa0dcb3fb7e59748faa38
Nina Yang Aperture <nina@aperture.finace>
2023-03-04 17:04:31 -0700

    fix build

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,22 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
 	reactStrictMode: true,
-	webpack: (config) => {
-		config.module.rules.push({
-			test: /node_modules\/@spectral-finance/,
-			resolve: {
-				alias: {
-					"../../styles/Icon.css": false,
-          "../styles/variables.css": false,
-          "../../styles/variables.css": false,
-          "../../../../styles/variables.css": false,
-          "react-circular-progressbar/dist/styles.css": false,
-          
-				},
-			},
-		});
-
-		return config;
-	},
 };
 export default nextConfig
