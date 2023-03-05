17727e4ddb514869479e507e21142966df01c184
Nina Yang Aperture <nina@aperture.finace>
2023-03-04 18:03:51 -0700

    updates

---
+++ frontend/views/Home/RangeOrder.tsx
@@ -0,0 +1,101 @@
+import { Box, Title } from "@aperture/uikit";
+import { Swap } from "./Swap";
+import styled from "styled-components";
+import { Positions } from "./Positions";
+import { SMBtn } from "@aperture/uikit";
+import { useAccount, useDisconnect } from "wagmi";
+import ConnectWallets from "@/components/Wallet/ConnectWallets";
+import { getstrategyAddress, getTokenAddress } from "@/config/contracts";
+import { useFetchUserToken } from "@/hooks/useFetchUserToken";
+import { useFetchUserPositions } from "@/hooks/useFetchUserPosition";
+import { useEffect, useState } from "react";
+import { useInView } from 'react-hook-inview';
+import SmoothList from 'react-smooth-list';
+
+const StyledBox = styled(Box)`
+  max-width: 1000px;
+  width: 100%;
+  height: 521px;
+  margin: auto;
+  margin-top: 10%;
+  background-color: white;
+  box-shadow: 0px 8px 16px 0px rgba(0, 0, 0, 0.3);
+`;
+const Wrapper = styled.div`
+  display: grid;
+  grid-template-columns: 50% 50%;
+`;
+const HR = styled.hr`
+  border-top: 1px solid ${({ theme }) => theme.colors.gray2};
+  width: calc(100% - 32px);
+`;
+const StyledTitle = styled(Title)`
+  width: 100%;
+  text-align: center;
+  position: relative;
+  font-family: "Chakra Petch", sans-serif;
+  font-size: 24px;
+`;
+const StyledSMBtn = styled(SMBtn)`
+  position: absolute;
+  right: 0;
+  top: -5px;
+`;
+
+const StyledConnectWallets = styled(ConnectWallets)`
+  margin: auto;
+  width: fit-content;
+`;
+
+export const RangeOrder = () => {
+  
+  const { disconnect } = useDisconnect();
+  const { isConnected } = useAccount();
+  const { mutate: wethMutate} = useFetchUserToken( getTokenAddress("weth", 80001), getstrategyAddress("limitOrder", 80001));
+  const { mutate: usdcMutate} = useFetchUserToken( getTokenAddress("usdc", 80001), getstrategyAddress("limitOrder", 80001));
+  const { mutate: positionMutate } = useFetchUserPositions(getstrategyAddress("limitOrder", 80001));
+
+  useEffect(() => {
+    async function mutate() {
+      wethMutate()
+      usdcMutate()
+      positionMutate()
+    }
+    const intervalId = setInterval(() => {
+      mutate()
+    }, 1000) // in milliseconds
+    return () => clearInterval(intervalId)
+  }, [])
+
+  const [ref, isVisible] = useInView({threshold: 0.5})
+  const [view, setView] = useState(false);
+  useEffect(() => {
+  		setTimeout(() => {
+  			isVisible ? setView(true) : setView(false)
+  		}, 100)
+  }, [isVisible])
+  return (
+    <span ref={ref}>
+      <SmoothList transitionDuration={1000} delay={100} visible={view}>
+    <StyledBox>
+      <StyledTitle>
+        UniV3 Range Order
+        {isConnected /*  @ts-ignore */ && (
+          <StyledSMBtn onClick={disconnect}>Disconnect</StyledSMBtn>
+        )}
+      </StyledTitle>
+      {/* <button>Connect</button> */}
+      <HR />
+      {isConnected ? (
+        <Wrapper>
+          <Swap />
+          <Positions />
+        </Wrapper>
+      ) : (
+        <StyledConnectWallets />
+      )}
+    </StyledBox>
+    </SmoothList>
+    </span>
+  );
+};
--- frontend/views/Home/productsLisr.tsx
+++ frontend/views/Home/productsLisr.tsx
@@ -2,19 +2,24 @@ import { EthereumIcon, AvalancheIcon, UsdtEIcon } from "@/packages/assetkit/src/
 const styles = {width:'100%', height:'100%', position: 'absolute', zIndex:'0', opacity:'1', right:'-40%', bottom:'-30%'}
 
 export const productsList = [
+    // {
+    //   networkIcon: <UsdtEIcon style={styles} />,
+    //   title: "Terra: PDN",
+    //   description: "i cant write shit, pls help me",
+    // },
+    // {
+    //   networkIcon: <AvalancheIcon style={styles} />,
+    //   title: "PDN: Crab market",
+    //   description: "i cant write shit, pls help me",
+    // },
+    // {
+    //   networkIcon: <AvalancheIcon style={styles} />,
+    //   title: "PDN: Crab market",
+    //   description: "i cant write shit, pls help me",
+    // },
     {
-      networkIcon: <UsdtEIcon style={styles} />,
-      title: "Terra: PDN",
-      description: "i cant write shit, pls help me",
-    },
-    {
-      networkIcon: <AvalancheIcon style={styles} />,
-      title: "PDN: Crab market",
-      description: "i cant write shit, pls help me",
-    },
-    {
-      networkIcon: <AvalancheIcon style={styles} />,
-      title: "PDN: Crab market",
+      networkIcon: <EthereumIcon style={styles} />,
+      title: "Univ3: Range Order",
       description: "i cant write shit, pls help me",
     },
     {
@@ -22,19 +27,14 @@ export const productsList = [
       title: "Univ3: Limit Order",
       description: "i cant write shit, pls help me",
     },
-    {
-      networkIcon: <EthereumIcon style={styles} />,
-      title: "Univ3: Amtomated Rebalance",
-      description: "i cant write shit, pls help me",
-    },
-    {
-      networkIcon: <EthereumIcon style={styles} />,
-      title: "Univ3: Take Profit & Stop Loss ",
-      description: "i cant write shit, pls help me",
-    },
-    {
-      networkIcon: <EthereumIcon style={styles} />,
-      title: "Univ3: DCA",
-      description: "i cant write shit, pls help me",
-    },
+    // {
+    //   networkIcon: <EthereumIcon style={styles} />,
+    //   title: "Univ3: Take Profit & Stop Loss ",
+    //   description: "i cant write shit, pls help me",
+    // },
+    // {
+    //   networkIcon: <EthereumIcon style={styles} />,
+    //   title: "Univ3: DCA",
+    //   description: "i cant write shit, pls help me",
+    // },
   ];
